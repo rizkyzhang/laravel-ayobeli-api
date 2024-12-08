0223768b5bc39857a53235876045352dab9e9a15
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-08 21:05:48 +0700

    update

--- deployment/deploy-to-staging.sh
+++ deployment/deploy-to-staging.sh
@@ -13,10 +13,10 @@ git pull origin develop
 #echo "Compose down"
 #docker compose -f docker-compose.yaml -f docker-compose.stg.yaml down
 ls -a
-echo "Compose up"
-docker compose up --build -d
+#echo "Compose up"
+#docker compose up --build -d
 #docker compose -f docker-compose.yaml -f docker-compose.stg.yaml up --build -d --remove-orphans
-echo "Run migrations"
-docker exec $DOCKER_CONTAINER_NAME php artisan migrate
-echo "Optimize application"
-docker exec $DOCKER_CONTAINER_NAME php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan optimize
+#echo "Run migrations"
+#docker exec $DOCKER_CONTAINER_NAME php artisan migrate
+#echo "Optimize application"
+#docker exec $DOCKER_CONTAINER_NAME php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan optimize
