799c6bba5cf77189213d43b5b57b931cf73196b4
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-08 20:53:40 +0700

    update

--- deployment/deploy-to-staging.sh
+++ deployment/deploy-to-staging.sh
@@ -10,10 +10,11 @@ set -x
 git checkout develop
 git pull origin develop
 
-echo "Compose down"
-docker compose -f docker-compose.yaml -f docker-compose.stg.yaml down
+#echo "Compose down"
+#docker compose -f docker-compose.yaml -f docker-compose.stg.yaml down
 echo "Compose up"
-docker compose -f docker-compose.yaml -f docker-compose.stg.yaml up --build -d --remove-orphans
+docker compose up --build -d
+#docker compose -f docker-compose.yaml -f docker-compose.stg.yaml up --build -d --remove-orphans
 echo "Run migrations"
 docker exec $DOCKER_CONTAINER_NAME php artisan migrate
 echo "Optimize application"
