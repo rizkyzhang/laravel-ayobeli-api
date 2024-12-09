82185b98879d27faf11d0f6eb1df4d72dea2fe40
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-09 21:49:42 +0700

    update

--- deployment/deploy-to-staging.sh
+++ deployment/deploy-to-staging.sh
@@ -11,10 +11,3 @@ echo "Compose down"
 docker compose -f docker-compose.yaml -f docker-compose.stg.yaml down
 echo "Compose up"
 docker compose -f docker-compose.yaml -f docker-compose.stg.yaml up --build -d --remove-orphans
-echo "APP_ENV $APP_ENV"
-echo "DB_CONNECTION: $DB_CONNECTION"
-echo "DB_USERNAME: $DB_USERNAME"
-echo "Run migrations"
-docker exec $DOCKER_CONTAINER_NAME php artisan migrate --force
-echo "Optimize application"
-docker exec $DOCKER_CONTAINER_NAME php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan optimize
