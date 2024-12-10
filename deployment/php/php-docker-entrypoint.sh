175f56852aa55a605435a8e02f35d9d0c87a5ab2
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-10 18:25:06 +0700

    update

--- deployment/php/php-docker-entrypoint.sh
+++ deployment/php/php-docker-entrypoint.sh
@@ -2,7 +2,12 @@
 set -e
 
 echo "🔄 Running migrations..."
-doppler run -- php artisan migrate --force
+if [[ $APP_ENV -ne "local" ]]; then
+    doppler run -- php artisan migrate --force
+else
+    php artisan migrate
+fi
+
 
 echo "✨ Clearing Laravel caches and optimizing..."
 php artisan config:clear
@@ -11,4 +16,8 @@ php artisan view:clear
 php artisan optimize
 
 echo "🚀 Starting php-fpm..."
-exec doppler run -- php-fpm
+if [[ $APP_ENV -ne "local" ]]; then
+    exec doppler run -- php-fpm
+else
+    exec php-fpm
+fi
