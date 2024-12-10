46deadcc979f6a8ee17d45c9ce23eea1a2447ab6
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-10 13:20:10 +0700

    update

--- deployment/php/php-docker-entrypoint.sh
+++ deployment/php/php-docker-entrypoint.sh
@@ -19,7 +19,7 @@ done
 #php artisan view:clear
 
 echo "🔄 Running migrations..."
-php artisan migrate --force
+doppler run -- php artisan migrate --force
 
 echo "🚀 Starting php-fpm..."
 exec php-fpm
