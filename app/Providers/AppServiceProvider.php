0180d695e5bdc97a6906d59428c4154082dad589
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-14 22:40:41 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewApiDocs', function (User $user) {
+        Gate::define('viewApiDocs', function () {
             return $this->app->environment() !== 'production';
         });
     }
