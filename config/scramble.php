54df771225fa566fae1c34237e13203783fa4aa5
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-14 23:15:26 +0700

    update

--- config/scramble.php
+++ config/scramble.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\RestrictApiDocsAccess;
+
 return [
     /*
      * Your API path. By default, all routes starting with this path will be added to the docs.
@@ -78,7 +80,7 @@ return [
 
     'middleware' => [
         'web',
-//        RestrictApiDocsAccess::class
+        RestrictApiDocsAccess::class
     ],
 
     'extensions' => [],
