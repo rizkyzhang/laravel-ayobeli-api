936e6d8f939fdc26e973867b755c75bf3947c160
rizkyzhang <rizkyzhangdev@gmail.com>
2024-12-14 20:14:13 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\ProductController;
 use App\Http\Middleware\VerifyRequestSignature;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(app()->environment() === 'local' ? [] : [VerifyRequestSignature::class])->group(function () {
+Route::middleware(in_array(app()->environment(), ['local', 'testing'])
+    ? [] : [VerifyRequestSignature::class])->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
         Route::post('login', [AuthController::class, 'login']);
